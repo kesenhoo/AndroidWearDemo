b15a9019615c3fa43d192c236cb8f3fefedb2277
kesenhoo <kesenhoo@gmail.com>
2014-07-16 00:22:55 +0800

    commit myActivity

--- mobile/src/main/java/com/tencent/weardemo/MyActivity.java
+++ mobile/src/main/java/com/tencent/weardemo/MyActivity.java
@@ -1,10 +1,15 @@
 package com.tencent.weardemo;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 
 public class MyActivity extends Activity {
@@ -17,6 +22,27 @@ public class MyActivity extends Activity {
         setContentView(R.layout.activity_my);
 
         mTextView = (TextView) findViewById (R.id.action_settings);
+
+        int notificationId = 001;
+        // Build intent for notification content
+        Intent viewIntent = new Intent(this, ViewEventActivity.class);
+        //viewIntent.putExtra(EXTRA_EVENT_ID, eventId);
+        PendingIntent viewPendingIntent =
+                PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.wear_icon)
+                        .setContentTitle("测试Title")
+                        .setContentText("测试Content")
+                        .setContentIntent(viewPendingIntent);
+
+        // Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+        // Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
     }
 
 
